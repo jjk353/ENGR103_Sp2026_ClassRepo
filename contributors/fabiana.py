c116752b0d2498a6db50abba42144136aa3ea39e
Alexa Fabian <fabiana@oregonstate.edu>
2026-06-04 09:30:58 -0700

    Add contributor file for fabiana

---
+++ contributors/fabiana.py
@@ -0,0 +1,9 @@
+# Contributor: Alexa Fabian
+# ONID: 934668082
+# Term: Spring 2026
+
+def introduce():
+    print("Hi, I’m Alexa Fabian, ONID 934668082.")
+    
+if __name__ == "__main__":
+    introduce()
