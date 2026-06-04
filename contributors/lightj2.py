6bb65efc2466cbd8a67726a30d920b3410ed4651
Jackson Light <lightj2@oregonstate.edu>
2026-06-04 09:23:54 -0700

    Add contributor file for lightj2

---
+++ contributors/lightj2.py
@@ -0,0 +1,10 @@
+#lightj2 file
+
+# Contributor: Jackson Light
+# ONID: lightj2
+# Term: Spring 2026
+def introduce():
+    print("Hi, I’m Jackson Light, ONID lightj2.")
+    
+if __name__ == "__main__":
+    introduce()
