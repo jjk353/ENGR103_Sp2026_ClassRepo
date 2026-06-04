df5dd49f11073bea9b0ce2bee0ea1a7e9acf90e2
Madix McFarland <mcfarmad@oregonstate.edu>
2026-06-04 15:06:30 -0700

    Add contributor file for 934687657

---
+++ contributors/934687657.py
@@ -0,0 +1,9 @@
+# Contributor : < Your Full Name >
+# ONID : < your - onid >
+# Term : < Term and Year >
+
+def introduce () :
+    print (" Hi , I’m Madix McFarland , ONID 934687657.")
+
+if __name__ == " __main__ ":
+    introduce ()
