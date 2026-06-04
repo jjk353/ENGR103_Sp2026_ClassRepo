5fea6fe4a1f4ad4ae96ce69c5f00928f2697195c
oregonstatecarson <baldwica@oregonstate.edu>
2026-06-04 09:37:17 -0700

    Add contributor file for baldwica

---
+++ contributors/baldwica.py
@@ -0,0 +1,8 @@
+# Contributor : Carson Baldwin-Bonney
+# ONID : baldwica
+# Term : Spring 2026
+def introduce() :
+    print(" Hi , I’m <Your Full Name> , ONID <your-onid>.")
+
+if __name__ == " __main__ " :
+    introduce()
