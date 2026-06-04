e10680183dc7901071b7b41098bf4354ef2b9c08
Ethan Stevenson <stevenet@oregonstate.edu>
2026-06-04 15:09:05 -0700

    Add contributor file for stevenet

---
+++ contributors/stevenet.py
@@ -0,0 +1,8 @@
+# Contributor : Ethan Stevenson
+# ONID : Stevenet
+# Term : Spring 2026
+def introduce():
+    print (" Hi, I’m Ethan Stevenson , ONID Stevenet.")
+
+if __name__ == "__main__":
+    introduce()
