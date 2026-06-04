2a2e8d61bb9d87c9e01a9c61ffb7697c84e41c67
Slade Watson <watsonsl@oregonstate.edu>
2026-06-04 13:13:38 -0700

    Add contributor file for watsonsl

---
+++ contributors/watsonsl.py
@@ -0,0 +1,9 @@
+# Contributor: <Slade Watson>
+# ONID:        <watsonsl>
+# Term:        <spring 2026>
+
+def introduce():
+    print("Hi, I’m Slade Watson, ONID watsonsl.")
+    
+if __name__ == "__main__":
+    introduce()
