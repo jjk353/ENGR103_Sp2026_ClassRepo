b52c19d747a064f28ce7ff1d88bb2d5ee7eef85a
Brennan Kornbau <kornbaub@oregonstate.edu>
2026-06-04 12:40:47 -0700

    Add contributor file for kornbaub

---
+++ contributors/kornbaub.py
@@ -0,0 +1,9 @@
+# Contributor: Brennan Kornbau
+# ONID: kornbaub
+# Term: Spring 2026
+
+def introduce():
+    print("Hi, I'm Brennan Kornbau, ONID kornbaub.")
+    
+if __name__ == "__main__":
+    introduce()
