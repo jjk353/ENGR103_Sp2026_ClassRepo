60b17a084aa706852e50a9aba9f9d2397e1ada0b
AdwaithRenjith <adwaithr2006@gmail.com>
2026-06-04 09:38:23 -0700

    Add contributor file for renjitha

---
+++ contributors/renjitha.py
@@ -0,0 +1,9 @@
+# Contributor : Adwaith Renjith
+# ONID : renjitha
+# Term : Spring 2026
+
+def introduce ():
+    print ("Hi , I’m Adwaith Renjith, ONID renjitha.")
+
+if __name__ == "__main__":
+    introduce ()
