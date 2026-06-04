3f0e446dfd4e356b1b7eb7d7933303c31f5dff4d
Kev <mattoxk@oregonstate.edu>
2026-06-04 09:38:39 -0700

    Add contributor file for mattoxk

---
+++ contributors/mattoxk.py
@@ -0,0 +1,8 @@
+# C o n t r i b u t o r : Kevin Mattox
+# ONID : mattoxk
+# Term : Spring 2026
+def introduce () :
+print (" Hi , I'm Kevin Mattox, ONID mattoxk.")
+
+if __name__ == "__main__":
+introduce ()
