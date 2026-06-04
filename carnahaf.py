5287f49c899dd1bdcb2d0a484d7556b92c41808a
Finn Carnahan <carnahaf@oregonstate.edu>
2026-06-04 09:21:06 -0700

    Add contributor file for carnahaf

---
+++ carnahaf.py
@@ -0,0 +1,8 @@
+# Contributor : Finn Carnahan
+# ONID : carnahaf@oregonstate.edu
+# Term : Spring 2026
+def introduce():
+    print ("Hi, I’m Finn Carnahan, ONID carnahaf.")
+
+if __name__ == " __main__ ":
+    introduce ()
