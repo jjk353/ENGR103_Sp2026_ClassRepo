1c2c8e45dfc493c42f363317ac8deec3161e7bca
Julia Melville <melvillj@oregonstate.edu>
2026-06-04 15:07:50 -0700

    Add contributor file for melvillj

---
+++ contributors/melvillj.py
@@ -0,0 +1,8 @@
+ #Contributor: Julia Melville
+# ONID: melvillj
+# Term: Spring 2026
+
+def introduce():
+    print("Hi, I’m Julia Melville, ONID melvillj.")
+    
+if __name__ == "__main__": introduce()
