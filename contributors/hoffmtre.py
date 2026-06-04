a42ad6d290458bb617d928ef104d6284d5e75618
Trevsteel <Trevorh2025@gmail.com>
2026-06-04 09:26:53 -0700

    Add contributor file for your - onid

---
+++ contributors/hoffmtre.py
@@ -0,0 +1,7 @@
+# Contributor : < Your Full Name >
+# ONID : < your - onid >
+# Term : < Term and Year >
+def introduce () :
+    print (" Hi , I ’ m < Trevor Hoffmann> , ONID <hoffmtre>.")
+if __name__ == "__main__":
+    introduce ()
