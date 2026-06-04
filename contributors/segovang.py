38e6be8f5fe66f9ed824788bb99fbb7bf4c966e0
Angel Segovia Gutierrez <angelsegov06@gmail.com>
2026-06-04 15:05:28 -0700

    Add contributor file for segovang

---
+++ contributors/segovang.py
@@ -0,0 +1,9 @@
+# Contributor: <Angel Segovia Gutierrez>
+# ONID: <segovang>
+# Term: <Spring 2026>
+
+def introduce():
+    print("Hi, I’m Angel Segovia Gutierrez, ONID segovang.")
+
+if __name__ == "__main__":
+    introduce()
