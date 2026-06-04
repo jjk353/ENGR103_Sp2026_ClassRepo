76e772bd2c65149660946c103818215c5cdd7766
Daniel Okonkwo <okonkwod@oregonstate.edu>
2026-06-04 14:57:34 -0700

    Add contributor file for okonkwod

---
+++ contributors/okonkwod.py
@@ -0,0 +1,11 @@
+# Contributor: <Daniel Okonkwo>
+# ONID:        <okonkwod@oregonstate.edu>
+# Term:        <Spring 2026>
+
+
+def introduce():
+    print("Hi, I'm <Daniel Okonkwo> ONID <okonkwod@oregonstate.edu>.")
+    
+if __name__ == "__main__":
+    introduce()
+        
