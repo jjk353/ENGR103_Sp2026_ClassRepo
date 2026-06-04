7965004c2e4bb818cf9d4346144798f9292e8adf
Joseph Sokol <jcsokol13@gmail.com>
2026-06-04 15:14:25 -0700

    Add contributor file for sokoj

---
+++ contributors/sokolj.py
@@ -0,0 +1,8 @@
+# Contributor : < Joseph Sokol >
+# ONID : < 934669878 >
+# Term : < spring 2026 >
+def introduce ():
+    print ("Hi, I’m Joseph Sokol, ONID-934669878.")
+
+if __name__ == "__main__":
+    introduce ()
